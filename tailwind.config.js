1915a00007334fe4781eec5f20d83612f7c9b61e
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2024-05-14 13:52:54 -0300

    [chore]: alterações na configuração do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+      },
+      boxShadow: {
+        'login-card': '0px 0px 64px 0px #00000040',
+      },
+      colors: {
+        'primary-blue': '#02274F',
+        'primary-yellow': '#FDCF00',
+        'greyish-white': '#fafafa',
+        'input-grey': '#f1f1f1',
+      },
+    },
   },
   plugins: [],
 };
